0cf7ae41e8777a0425a528b107dbd2814173c804
Vínicius Santos <viniciuszero@Live.com>
2023-05-09 19:42:47 -0300

    :sparkles: (controllers) Creating application con…

---
+++ src/controllers/pokemonsController.js
@@ -0,0 +1,115 @@
+const PokemonModel = require('../model/pokemon.model')
+
+const create = async (req, res) => {
+  const { name, attack, defense, speed, hp, type1, type2, is_legendary } = req.body
+
+  const pokemonAlreadyExists = await PokemonModel.findOne({ name: name })
+
+  if (pokemonAlreadyExists) {
+    return res.status(400).json({
+      error: '@pokemons/create',
+      message: 'Pokemon already exists'
+    })
+  }
+
+  const pokedex_number = await PokemonModel.countDocuments() + 1
+
+  try {
+    const pokemon = await PokemonModel.create({
+      name,
+      attack,
+      defense,
+      speed,
+      pokedex_number,
+      hp,
+      type1,
+      type2,
+      is_legendary,
+    })
+
+    return res.status(201).json(pokemon)
+  } catch (err) {
+    return res.status(400).json({
+      error: '@pokemons/create',
+      message: err.message || 'Failed to create pokemon'
+    })
+  }
+}
+
+const list = async (req, res) => {
+  const queryParams = req.query
+
+  try {
+    const pokemons = await PokemonModel.find(queryParams)
+
+    if (pokemons.length < 1) {
+      throw new Error("Couldn't find a pokemon")
+    }
+
+    return res.json(pokemons)
+  } catch (err) {
+    return res.status(400).json({
+      error: '@pokemons/list',
+      message: err.message || "failed to list pokemon"
+    })
+  }
+}
+
+const update = async (req, res) => {
+  const { id } = req.params
+  const { name, attack, defense, speed, hp, type1, type2, is_legendary } = req.body
+
+  const updatedPokemon = {
+    name,
+    attack,
+    defense,
+    speed,
+    type1,
+    type2,
+    hp,
+    is_legendary
+  }
+
+  try {
+    await PokemonModel.findByIdAndUpdate(id, updatedPokemon)
+
+    const pokemon = await PokemonModel.findById(id)
+
+    return res.json(pokemon)
+  } catch (err) {
+    return res.status(400).json({
+      error: '@pokemons/update',
+      message: err.message || "failed to update pokemon"
+    })
+  }
+}
+
+const remove = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const pokemonDeleted = await PokemonModel.findByIdAndRemove(id)
+
+    if (!pokemonDeleted) {
+      return res.status(400).json({
+        error: '@pokemons/remove',
+        message: "invalid pokemon id"
+      })
+    }
+
+    return res.json({})
+  } catch (err) {
+    return res.status(400).json({
+      error: '@pokemon/remove',
+      message: err.message || "error deleting the pokemon"
+    })
+  }
+
+}
+
+module.exports = {
+  list,
+  create,
+  update,
+  remove
+}
---
+++ src/controllers/treinersController.js
@@ -0,0 +1,125 @@
+const TreinerModel = require('../model/treiner.model')
+const PokemonModel = require('../model/pokemon.model');
+
+const create = async (req, res) => {
+  const { name, age, location, is_leader, badges, speciality, pokemons } = req.body
+
+  try {
+    const pokemonsDetailed = await PokemonModel.find({
+      '_id': {
+        $in: pokemons
+      }
+    });
+
+    if (pokemonsDetailed.length == 0) {
+      return res.status(400).json({
+        error: '@treiners/create',
+        message: "Hey! Wait! Wild Pokémon live in tall grass! You need your own Pokémon for your protection"
+      })
+    }
+
+    const treiner = await TreinerModel.create({
+      name,
+      age,
+      location,
+      is_leader,
+      badges,
+      speciality,
+      pokemons: pokemonsDetailed
+    })
+
+    return res.status(201).json(treiner)
+  } catch (err) {
+    return res.status(400).json({
+      error: '@treiners/create',
+      message: err.message || 'Failed to create treiner'
+    })
+  }
+}
+
+const list = async (req, res) => {
+  const queryParams = req.query
+
+  try {
+    const treiners = await TreinerModel.find(queryParams)
+
+    return res.json(treiners)
+  } catch (err) {
+    return res.status(400).json({
+      error: '@treiners/list',
+      message: err.message || 'Failed to list treiners'
+    })
+  }
+}
+
+const update = async (req, res) => {
+  const { id } = req.params
+  const { name, age, location, is_leader, badges, speciality, pokemons } = req.body
+
+  try {
+    const treinerExists = await TreinerModel.findById(id)
+
+    if (!treinerExists) {
+      return res.status(400).json({
+        error: "@treiners/update",
+        message: `Treiner not found ${id}`
+      })
+    }
+
+    const pokemonsDetailed = await PokemonModel.find({
+      '_id': {
+        $in: pokemons
+      }
+    });
+
+    const updatedTreiner = {
+      name,
+      age,
+      location,
+      is_leader,
+      badges,
+      speciality,
+      pokemons: pokemons ? pokemonsDetailed : undefined
+    }
+
+    await TreinerModel.findByIdAndUpdate(id, updatedTreiner)
+
+    const treiner = await TreinerModel.findById(id)
+
+    return res.json(treiner)
+  } catch (err) {
+    return res.status(400).json({
+      error: '@treiners/update',
+      message: err.message || 'Failed to update treiner'
+    })
+  }
+}
+
+const remove = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const deletedTreiner = await TreinerModel.findByIdAndRemove(id)
+
+    if (!deletedTreiner) {
+      return res.status(400).json({
+        error: '@pokemons/remove',
+        message: "invalid treiner id"
+      })
+    }
+
+    return res.json({})
+  } catch (err) {
+    return res.status(400).json({
+      error: '@treiner/remove',
+      message: err.message || "error deleting the treiner"
+    })
+  }
+}
+
+module.exports = {
+  list,
+  create,
+  update,
+  remove
+}
